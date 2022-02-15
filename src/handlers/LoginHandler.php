b3cbcf14de0f9f9e48c48fa26c809c8c579c27a6
Renan-Gust <renandevfullstack@gmail.com>
2022-02-15 11:53:46 -0300

    login-register-partials

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -4,7 +4,7 @@ namespace src\handlers;
 use \src\models\User;
 
 class LoginHandler {
-    public static function checkLogin() {
+    public static function checkLogin() { //Verifica se está logado
         if(!empty($_SESSION['token'])) {
             $token = $_SESSION['token'];
             $data = User::select()->where('token', $token)->one();
@@ -12,8 +12,8 @@ class LoginHandler {
             if(count($data) > 0){
                 $loggedUser = new User();
                 $loggedUser->id = $data['id'];
-                $loggedUser->email = $data['email'];
                 $loggedUser->name = $data['name'];
+                $loggedUser->avatar = $data['avatar'];
 
                 return $loggedUser;
             }
@@ -39,5 +39,26 @@ class LoginHandler {
         }
 
         return false;
+    }
+
+    public static function emailExists($email){
+        $user = User::select()->where('email', $email)->one();
+
+        return $user ? true : false;
+    }
+
+    public static function addUser($name, $email, $password, $birthdate){
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $token = md5(time().rand(0, 9999).time());
+
+        User::insert([
+            "email" => $email,
+            "password" => $hash,
+            "name" => $name,
+            "birthdate" => $birthdate,
+            "token" => $token
+        ])->execute();
+
+        return $token;
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -5,5 +5,13 @@ $router = new Router();
 
 $router->get('/', 'HomeController@index');
 $router->get('/login', 'LoginController@signin');
-$router->post('/login', 'LoginController@signinInAction');
+$router->post('/login', 'LoginController@signinAction');
 $router->get('/cadastro', 'LoginController@signup');
+$router->post('/cadastro', 'LoginController@signupAction');
+
+// $router->get('/pesquisa')
+// $router->get('/perfil')
+// $router->get('/sair')
+// $router->get('/amigos')
+// $router->get('/fotos')
+// $router->get('/config')
